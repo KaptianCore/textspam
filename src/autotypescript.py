34260dda6f2166b5dd4608762b8d7c870c3777af
Kaptian Core <kaptiancore@gmail.com>
2021-08-10 11:00:52 +1000

    Remove Manual Array Add Auto Picking From Files In current_directory/scripts

--- src/autotypescript.py
+++ src/autotypescript.py
@@ -4,9 +4,9 @@ import keyboard
 import os
 cwd = os.getcwd()
 time.sleep(5)
-scripts = ["bee","shrek","strictlyballroom","emoji","rickroll"]
+scripts = os.listdir(f"{cwd}/scripts")
 n = open(f"{cwd}/scripts/{random.choice(scripts)}", 'r')
-# n = open(f"{cwd}/scripts/beemoviescriptthing/pi", 'r')
+# n = open(f"{cwd}/scripts/pi", 'r')
 for word in n:
     if keyboard.is_pressed('ESC'):
         print('Exiting Random Script Spam')
